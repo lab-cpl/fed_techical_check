2711489b6cd4d2635c733c9d29047827e936a2e7
nicolasluarte <l.luarte@uc.cl>
2023-03-06 14:10:50 -0300

    battery test done

--- scripts/analysis/fed_batt_analysis.R
+++ scripts/analysis/fed_batt_analysis.R
@@ -1,3 +1,25 @@
-# Take data containing number of motor turns a battery voltage
-# plots a curve over time and compares it to standard or
-# expected curve
+# Corre esta linea para encontrar el archivo de carga de bateria
+file_name <- file.choose()
+
+# Corre estas lineas para producir el grafico de carga de bateria
+# recuerda que debes instalar el paquete pacman!
+pacman::p_load(
+    tidyverse,
+    ggplot2
+)
+raw <- read_csv(file_name)
+df <- raw %>% 
+    select(time, battery) %>% 
+    mutate(
+        time = time - lag(time, default = NA),
+        time = replace_na(time, 0),
+        time_acc = cumsum(time)
+    )
+p1 <- df %>% 
+    ggplot(aes(
+        time_acc, battery
+    )) +
+    geom_line() +
+    theme_bw() +
+    ylab("Bateria") +
+    xlab("Tiempo")
